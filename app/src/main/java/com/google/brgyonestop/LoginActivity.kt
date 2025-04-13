893db8a8cdedb490009eedf569e96d028e62e825
SawDirk <130374565+IgotW@users.noreply.github.com>
2025-04-13 13:54:53 +0800

    Igot Small Changes

--- app/src/main/java/com/google/brgyonestop/LoginActivity.kt
+++ app/src/main/java/com/google/brgyonestop/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.google.brgyonestop
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,6 +17,7 @@ class LoginActivity : Activity() {
 
         val textview_forgotpassword = findViewById<TextView>(R.id.textview_forgotpassword)
         val textview_register = findViewById<TextView>(R.id.textview_register)
+        val button_login = findViewById<Button>(R.id.button_login)
 
         textview_register.setOnClickListener {
             startActivity(
@@ -26,6 +28,11 @@ class LoginActivity : Activity() {
         textview_forgotpassword.setOnClickListener {
             startActivity(
                 Intent(this, ForgotPasswordActivity::class.java)
+            )
+        }
+        button_login.setOnClickListener {
+            startActivity(
+                Intent(this, DashboardActivity::class.java)
             )
         }
     }
