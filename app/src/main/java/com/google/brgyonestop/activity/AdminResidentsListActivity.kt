78e5222d1f739d858da19cdc0ff02188a0a4f2b3
SawDirk <130374565+IgotW@users.noreply.github.com>
2025-04-27 00:26:04 +0800

    Changes on Purok List & Add Resident List

--- app/src/main/java/com/google/brgyonestop/activity/AdminResidentsListActivity.kt
+++ app/src/main/java/com/google/brgyonestop/activity/AdminResidentsListActivity.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.brgyonestop.R
 
-class AdminTotalResidentsActivity : AppCompatActivity() {
+class AdminResidentsListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_admin_total_residents)
+        setContentView(R.layout.activity_admin_residents_list)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
